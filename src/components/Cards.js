83d4b422889693ee130f36e3ef482173b03e442c
Axel <axel.etoile.espoire@gmail.com>
2022-05-10 16:49:36 +0200

    fix

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,12 @@ const Cards = () => {
     const data = useSelector((state) => state.weather.data);
     return (
         <div>
-            <h1>{(data.name === undefined) ? "Recherche une ville": data.name+', '+data.main.temp+'°, ressenti '+data.main.feels_like+'° ('+data.main.temp_min+"° min & "+data.main.temp_max+"° max), "+data.weather[0].description }</h1>
+            <h1>Maintenant :</h1>
+            <h2>{(data.name === undefined) ? "Recherche une ville": data.name+', '+data.main.temp+'°, ressenti '+
+            data.main.feels_like+'° ('+data.main.temp_min+"° min & "+data.main.temp_max+"° max), "+
+            data.weather[0].description }</h2>
+            <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}/>
+            <br/>
         </div>
     )
 }
