b51b543c2aa7d16a60b7926ffb140110155308d2
Axel <axel.etoile.espoire@gmail.com>
2022-05-11 16:56:31 +0200

    Add carousel + fix other

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Row, Container, Col, Card, ListGroup, ListGroupItem, InputGroup, FormControl, Button } from 'react-bootstrap'
+import { Row, Container, Col, Card, ListGroup, ListGroupItem, Carousel } from 'react-bootstrap'
 import moment from "moment-timezone";
 import frLocal from "moment/locale/fr";
 
@@ -12,34 +12,30 @@ const Cards = () => {
     function time(datetime, format) {
         moment.updateLocale('fr', frLocal);
         var time1 = moment(datetime).tz("Europe/Paris").format(format)
-        // console.log(time1)
         return time1
     }
 
 
     return (
         <div>
-            {/* {console.log(data_week.list[0].dt)} */}
-
-
             <Container>
                 <Row>
-                    <h1>{(weather_check) ? "Recherche une ville" : "Maintenant sur : "+data.name}</h1>
+                    <h1>{(weather_check) ? "Recherche une ville" : "Maintenant sur : " + data.name}</h1>
                     <h2>{(weather_check) ? "" : data.main.temp + '°, ressenti de ' +
                         data.main.feels_like + '°, ' +
                         data.weather[0].description}</h2>
-                    {/* {(weather_check) ? " " : <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} />} */}
                 </Row>
                 <Row>
                     {(data_week === undefined) ? " " : data_week.list.map((el, index) => {
                         // console.log(el)
-                        return (
-                            <Col style={{ padding: '0px 0px 40px 0px' }}>
+                        if(index !== 0){
+                            return (
+                                <Col style={{ padding: '0px 0px 40px 0px' }}>
                                 <Card key={index}
                                     bg={variant.toLowerCase() === 'light' ? 'white' : 'dark'} border={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
                                     style={{ width: '18rem' }}
                                     text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}>
-                                    <Card.Img style={{width: '150px',heigth:"150px"}} variant="top" src={`http://openweathermap.org/img/wn/${el.weather[0].icon}.png`} />
+                                    <Card.Img style={{ width: '150px', heigth: "150px" }} variant="top" src={`http://openweathermap.org/img/wn/${el.weather[0].icon}.png`} /> 
                                     <Card.Body>
                                         <Card.Title>{time(data_week.list[index].dt_txt, "DD MMMM YYYY à HH:mm")}</Card.Title>
                                         <Card.Text>
@@ -56,9 +52,30 @@ const Cards = () => {
                                 </Card>
                             </Col>
                         )
+                    }
                     })}
                 </Row>
             </Container>
+            {/* <Carousel>
+                {(data_week === undefined) ? " " : data_week.list.map((el, index) => {
+                    // console.log(el)
+                    return (
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={`http://openweathermap.org/img/wn/${el.weather[0].icon}.png`}
+                                style={{height:'50rem'}}
+                                alt={el.weather[0].description}
+                            />
+                            <Carousel.Caption
+                            style={{color:'#000'}}>
+                                <h3>{time(data_week.list[index].dt_txt, "DD MMMM YYYY à HH:mm")}</h3>
+                                <p>{el.main.temp}°, ressenti de {el.main.feels_like}°</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )
+                })}
+            </Carousel> */}
 
         </div>
     )
